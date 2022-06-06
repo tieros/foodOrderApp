785804582d536c67bc3aea68bb5ce93307bb1333
tieros <anonimityisfirst@gmail.com>
2022-06-06 16:54:00 +0300

    feat: add animated cart icon

--- src/organisms/navbar/index.js
+++ src/organisms/navbar/index.js
@@ -1,19 +1,46 @@
-import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import cartIcon from '../../assets/cartIcon.svg';
 
-export default function Navbar(){
+export default function Navbar() {
+    const [iconAnimated, setIconAnimated] = useState(false);
 
-const isLoggedIn = useSelector((state) => state.auth.user.isLoggedIn);
+    const isLoggedIn = useSelector((state) => state.auth.user.isLoggedIn);
+    const cartItems = useSelector((state) => state.cart.items);
+
+    useEffect(() => {
+        if (cartItems.length === 0) {
+            return;
+        }
+        setIconAnimated(true);
+
+        const timer = setTimeout(() => {
+            setIconAnimated(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [cartItems]);
+
+    const iconClass = `cart-icon ${iconAnimated ? 'bump' : ''}`;
+
     return (
         <nav className='navbar'>
             <p className='logo'>Logo</p>
             <ul>
-                <Link to='/'> Menu </Link>
-                { isLoggedIn ? 
-                    <Link to='profile'> Profile </Link> : 
-                    <Link to='login'> Login </Link>
-                }
-                <Link to='cart'> Cart </Link>
+                <NavLink to='/'> Menu </NavLink>
+                {isLoggedIn ? (
+                    <NavLink to='profile'> Profile </NavLink>
+                ) : (
+                    <NavLink to='login'> Login </NavLink>
+                )}
+                <NavLink to='cart' className='cart-link'>
+                    {' '}
+                    Cart
+                    <img src={cartIcon} className={iconClass} alt='cart-icon' />
+                </NavLink>
             </ul>
         </nav>
     );
