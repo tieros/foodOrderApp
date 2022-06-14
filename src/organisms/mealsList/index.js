2ace4b81bc3bbd40d3cce5757497e891ccae6a88
tieros <anonimityisfirst@gmail.com>
2022-06-14 15:10:44 +0300

    fix: menu page

--- src/organisms/mealsList/index.js
+++ src/organisms/mealsList/index.js
@@ -1,50 +1,16 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
 import style from './mealsList.module.scss';
-import Card from '../../atoms/card';
 import MealItem from '../../molecules/mealtem';
-import { ref, onValue, child } from 'firebase/database';
-import { database } from '../../firebase';
-import { useDispatch, useSelector } from 'react-redux';
-import { mealActions } from '../../store/meals';
 
-export default function MealsList() {
+export default function MealsList(props) {
 
-    const reduxMeals = useSelector(state => state.meal.meals);
-
-    const [meals, setMeals] = useState(reduxMeals);
-    const [isLoading, setIsLoading] = useState();
+    const isLoading = useSelector((state) => state.ui.isLoading);
     const [error, setError] = useState(null);
 
-    const dispatch = useDispatch();
+    const { data } = props;
 
-    useEffect(() => {
-        if (reduxMeals.length === 0) {
-            setIsLoading(true);
-            const dbRef = ref(database);
-
-            onValue(child(dbRef, '/meals'), (snapshot) => {
-                const data = snapshot.val();
-                console.log(data);
-                const loadedMeals = [];
-
-                for (const key in data) {
-                    loadedMeals.push({
-                        id: key,
-                        name: data[key].name,
-                        description: data[key].description,
-                        price: data[key].price,
-                    });
-                }
-
-                console.log(loadedMeals);
-                dispatch(mealActions.setMeals(loadedMeals));
-                setMeals(loadedMeals);
-                setIsLoading(false);
-            });
-        }
-    }, [dispatch, meals, reduxMeals]);
-
-    if (meals === null) {
+    if (data === null) {
         setError('Error loading meals');
     }
 
@@ -64,7 +30,7 @@ export default function MealsList() {
         );
     }
 
-  const mealsList = meals.map((meal) => (
+  const mealsList = data.map((meal) => (
       <MealItem
           id={meal.id}
           key={meal.id}
--- src/pages/menu/index.js
+++ src/pages/menu/index.js
@@ -1,9 +1,46 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { mealActions } from "../../store/meals";
+import { database } from '../../firebase';
+import { ref, onValue, child } from "firebase/database";
+import { uiActions } from "../../store/ui";
 import MealsList from "../../organisms/mealsList";
 
 export default function Menu(){
+
+    const dispatch = useDispatch();
+    const reduxMeals = useSelector((state) => state.meal.meals);
+
+    const dbMeals = () => {
+        dispatch(uiActions.setIsLoading(true));
+        let meals = [];
+        const dbRef = ref(database);
+        onValue(child(dbRef, '/meals'), (snapshot) => {
+            const data = snapshot.val();
+            console.log(data);
+
+            for (const key in data) {
+                meals.push({
+                    id: key,
+                    name: data[key].name,
+                    description: data[key].description,
+                    price: data[key].price,
+                });
+            }
+
+            console.log(meals);
+            dispatch(mealActions.setMeals(meals));
+            setMeals(meals);
+            dispatch(uiActions.setIsLoading(false));
+        });
+        return meals;
+    };
+    
+    const [meals, setMeals] = useState(reduxMeals ? reduxMeals : dbMeals());
+
     return (
         <div className='menu-cont'>
-            <MealsList />
+            <MealsList data={meals}/>
         </div>
     );
 }
